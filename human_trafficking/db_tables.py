05011c37bd8038cd4ed20a7742665a199c8192e3
Fernando Carrillo <fernando@carrillo.at>
2015-09-24 20:52:32 -0400

    Added Location and  Message Tables in database. Modified Network_search class to take any object of subclass User_base

--- human_trafficking/db_tables.py
+++ human_trafficking/db_tables.py
@@ -1,6 +1,6 @@
 import os 
 import sys
-from sqlalchemy import Column, ForeignKey, Boolean, String, Integer 
+from sqlalchemy import Column, ForeignKey, Boolean, String, Integer, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship
@@ -15,6 +15,7 @@ class User(Base):
 	__tablename__ = 'user'
 	name = Column(String(50), primary_key=True)
 	visited = Column(Boolean, nullable=False)
+	depth = Column(Integer, nullable=False)
 
 class Connection(Base): 
 	"""
@@ -30,6 +31,30 @@ class Connection(Base):
 	user_1 = relationship("User", foreign_keys=[user_1_name])
 	user_2 = relationship("User", foreign_keys=[user_2_name])
 
+class Location(Base):
+	"""
+	Set-up location table: 
+	id, user_name, geojson, location  
+	"""
+	__tablename__ = 'location'
+	id = Column(Integer, primary_key=True)
+	user_name = Column(String(50), ForeignKey('user.name'))
+	geojson = Column(Text)
+	location = Column(Text)
+
+	user = relationship("User", foreign_keys=[user_name])
+
+class Message(Base): 
+	"""
+	Set-up location table: 
+	id, user_name, text
+	"""
+	__tablename__ = 'message'
+	id = Column(Integer, primary_key=True)
+	user_name = Column(String(50), ForeignKey('user.name'))
+	text = Column(Text)
+
+	user = relationship("User", foreign_keys=[user_name])
 
 # Create an engine that stores data in the local path 
 engine = create_engine('sqlite:///data/twitter_search.db')
--- human_trafficking/main.py
+++ human_trafficking/main.py
@@ -11,17 +11,26 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import exists
 
-from db_tables import Base, User, Connection
+from db_tables import Base, User, Connection, Message, Location
 
-class Twitter_search(object):
+class Network_search(object):
 	"""
 	Performs a search around a given starting username of depth n. 
 	"""
-	def __init__(self, user_name, db_session, depth=1): 
-		self.user_name = user_name
+	def __init__(self, user_object, db_session, depth=1): 
+		"""
+		@param user_object Subclass of User_base class
+		@param db_session database connection 
+		@depth depth of search. 
+		"""
+		self.user_object = user_object 
+		self.root_node = getattr(user_object, 'user_name')
 		self.db_session = db_session
-		self.depth = depth
-		self.add_user(user_name)
+		self.cur_depth = 0
+		self.add_user(self.root_node)
+		self.cur_depth += 1 
+		self.max_depth = depth
+		
 			
 	def add_user(self, user_name): 
 		"""
@@ -30,7 +39,7 @@ class Twitter_search(object):
 		2. If not, add. 
 		"""
 		if not self.db_session.query(exists().where(User.name == user_name)).scalar(): 
-			new_user = User(name=user_name, visited=False)
+			new_user = User(name=user_name, visited=False, depth=self.cur_depth)
 			self.db_session.add(new_user)
 			self.db_session.commit()
 
@@ -39,7 +48,7 @@ class Twitter_search(object):
 		Get the next user who has not been visited. 
 		"""
 		try:
-			return(self.db_session.query(User).filter(User.visited == False).one().name)
+			return(self.db_session.query(User).filter(User.visited == False).first().name)
 		except Exception, e:
 			return 
 
@@ -61,22 +70,56 @@ class Twitter_search(object):
 		self.db_session.add(new_connection)
 		self.db_session.commit()
 
+	def add_message(self, user_name, text): 
+		"""
+		Add message to MESSAGE relationship. 
+		"""
+		new_message = Message(user_name=user_name, text=text)
+		self.db_session.add(new_message)
+		self.db_session.commit()
+
+	def add_location(self, user_name, geojson, location): 
+		"""
+		Add message to LOCATION relationship. 
+		"""
+		print(str(geojson))
+		print(str(location))
+		
+		if (geojson == np.nan): geojson = "NULL"
+		if (location == np.nan): location = "NULL"
+		new_location = Location(user_name=user_name, geojson=geojson, location=location)
+		self.db_session.add(new_location)
+		self.db_session.commit()
+
 	def run(self, message_count=100, dump=True): 
 		"""
-		1. Connect to twitter api 
-		2. Download messages
-		3. Dump messages
+		1. Download and dump messages
+		2. Get nodes and add to users and connection tables. 
+		3. Get location and add to location table. 
 		"""
 		u1 = self.get_next_user()
-		user = Twitter_user(u1, Twitter_auth().authenticate())
-		user.load(message_count)
-		if (dump): user.dump('data/' + u1 + '.json')
+		setattr(self.user_object, 'user_name', u1)
+		#user = Twitter_user(u1, Twitter_auth().authenticate())
+		self.user_object.load(message_count)
+		if (dump): self.user_object.dump('data/' + u1 + '.json')
 
-		nodes = user.get_nodes()
+		for text in self.user_object.get_messages(): 
+			self.add_message(u1, text)
+
+		nodes = self.user_object.get_nodes()
 		for name, weight in nodes.iteritems(): 
 			u2 = name.strip('\@')
 			self.add_user(user_name=u2)
 			self.add_connection(user_name1=u1, user_name2=u2, weight=weight)
+
+		locations = self.user_object.get_locations()
+		print(locations)
+		for geojson, location in locations.iteritems(): 
+			print(geojson)
+			print(location)
+
+			#self.add_location(user_name=u1, geojson=row[0], location=row[1])
+
 		self.set_user_visited(user_name=u1)
 		
 if __name__ == '__main__':
@@ -87,7 +130,9 @@ if __name__ == '__main__':
 	DBSession = sessionmaker(bind=engine)
 	session = DBSession()
 
-	search = Twitter_search(user_name='fcarrillo81', db_session=session)
+	# Add root node user. 
+	user = Twitter_user('fcarrillo81', Twitter_auth().authenticate())
+	search = Network_search(user_object=user, db_session=session)
 	search.run()
 
 	# user = Twitter_user("test", Twitter_auth().authenticate())
