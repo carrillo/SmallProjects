5c1e365c49b7dab439dcc71b072a37a7bfd7a284
Fernando Carrillo <fernando@carrillo.at>
2015-09-30 12:06:47 -0400

    Save model for tweet classification

--- human_trafficking/tweet_classification.py
+++ human_trafficking/tweet_classification.py
@@ -1,5 +1,7 @@
 import pandas as pd 
 import numpy as np 
+import cPickle as pickle 
+import gzip 
 from matplotlib import pyplot as plt
 
 from sklearn.datasets import fetch_20newsgroups
@@ -8,29 +10,38 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
 
 from sklearn import metrics
 ###########################
 # Train a text classifier. 
 ###########################
-# Load toy data set. Replace training with sth different. 
-twenty_train = fetch_20newsgroups(subset='train', shuffle=True, random_state=32)
-twenty_test = fetch_20newsgroups(subset='test', shuffle=True, random_state=32)
+learn_model = True 
+if learn_model: 
+	# Load toy data set. Replace training with sth different. 
+	twenty_train = fetch_20newsgroups(subset='train', shuffle=True, random_state=32)
+	twenty_test = fetch_20newsgroups(subset='test', shuffle=True, random_state=32)
 
-# Generate bag-of-word representation and train classifier 
-# 1. Count occurances in the entire corpus
-# 2. tf-idf 
-#	a) Get term frequencies (normalizing to document length) -> term frequency tf
-#	b) Downscale weights which occur in many document -> inverse document frequency idf 
-steps = [('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('nb', MultinomialNB())] 
-clf = Pipeline(steps)
-param_grid = {'vect__ngram_range': [(1, 2)], 'nb__alpha': [10**-4,10**-3,10**-2]}
-grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, verbose=3, cv=5).fit(twenty_train.data, twenty_train.target)
-print( ('Best score %s of estimator %s') % (grid_search.best_score_, grid_search.best_estimator_))
+	# Generate bag-of-word representation and train classifier 
+	# 1. Count occurances in the entire corpus
+	# 2. tf-idf 
+	#	a) Get term frequencies (normalizing to document length) -> term frequency tf
+	#	b) Downscale weights which occur in many document -> inverse document frequency idf 
+	steps = [('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('nb', MultinomialNB())] 
+	clf = Pipeline(steps)
+	param_grid = {'vect__ngram_range': [(1, 2)], 'nb__alpha': [10**-4,10**-3,10**-2]}
+	grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, verbose=3, cv=5).fit(twenty_train.data, twenty_train.target)
+	print( ('Best score %s of estimator %s') % (grid_search.best_score_, grid_search.best_estimator_))
 
-# Fit the entire data to the best predictor. 
-clf = grid_search.best_estimator_
-#clf.fit(twenty_train.data, twenty_train.target)
+	# Fit the entire data to the best predictor. 
+	clf = grid_search.best_estimator_
+	clf.fit(twenty_train.data, twenty_train.target)
+	_ = joblib.dump(clf, 'data/tweet_clf.p', compress=9)
+else: 
+	print("loading")
+	clf = joblib.load('data/tweet_clf.p')
+	print("done")
+
 
 # Test on unseen data and report performance
 predicted = clf.predict(twenty_test.data)
